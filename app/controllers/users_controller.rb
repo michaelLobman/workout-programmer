744e7029ae71e3e7fc960eec781e5fea871b58ec
Michael Lobman <michael.lobman@gmail.com>
2022-04-17 09:24:46 -0400

    Add user routes and controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = find_user
+        render json: user
+    end
+
+    private
+
+    # do i need current week here? Or can that be supplied by the backend?
+
+    def user_params
+        params.permit(:full_name, :current_week)
+    end
+
+    def find_user
+        User.find(session[:user_id])
+    end
+
 end
---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,7 @@
+import Form from 'react-bootstrap/Form';
+
+function LoginForm(){
+    return(
+        
+    )
+}
