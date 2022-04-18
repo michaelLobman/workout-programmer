ae6b36edb081d8d9b5a655fb882fe1e45896db11
Michael Lobman <michael.lobman@gmail.com>
2022-04-18 15:07:30 -0400

    Add amrap functionality to respective sets

--- client/src/components/SetListItem.js
+++ client/src/components/SetListItem.js
@@ -5,19 +5,22 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 function SetListItem({ set, wMax, counter, setCounter }){
     const [completeBoolean, setCompleteBoolean] = useState(false)
-    const { reps, percentage } = set
+    const { num, reps, percentage, week_id: week } = set
 
     const liftingWeight = Math.round(wMax * percentage / 5) * 5
     const variant = completeBoolean ? 'success' : 'outline-success'
+    const amrap = num === 3 && week % 4 !== 0 ? '+' : null
 
     function handleClick(){
         setCompleteBoolean(!completeBoolean)
         setCounter(counter  => counter + 1)
     }
 
+
+
     return (
         <ListGroup.Item>
-            {reps} reps @ {liftingWeight} pounds
+            {reps} {amrap} reps @ {liftingWeight} pounds
             <Button 
                 className='set-button'
                 disabled={completeBoolean}
