99de2f0f7ef4d9548ae785e57e73d97ce22b175d
Michael Lobman <michael.lobman@gmail.com>
2022-04-18 21:41:17 -0400

    Add conditional rendering to SetListItem component"
    l

--- client/src/components/SetListItem.js
+++ client/src/components/SetListItem.js
@@ -4,19 +4,15 @@ import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function SetListItem({ set, wMax, completed, setProgressions, id, userId }){
-    const [completeBoolean, setCompleteBoolean] = useState(false)
     const [disableButton, setDisableButton] = useState(false)
     const { num, reps, percentage, week_id: week } = set
 
     const liftingWeight = Math.round(wMax * percentage / 5) * 5
-    // const variant = completeBoolean ? 'success' : 'outline-success'
     const amrap = num === 3 && week % 4 !== 0 ? '+' : null
-
     const variant = completed >= num ? 'success' : 'outline-success'
 
     function handleClick(e){
         e.preventDefault();
-    
         const data = {
             sets_completed: completed + 1
         }
