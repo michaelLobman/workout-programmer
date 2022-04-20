49c40de668f06911adf783d429310c40c78d9ff1
Michael Lobman <michael.lobman@gmail.com>
2022-04-20 09:09:59 -0400

    Configured client-side-routing

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get '*path',
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
+
 
 end
