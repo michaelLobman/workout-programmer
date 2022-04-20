8317bbc1c27f6b304b21af44db7be08dc7ed57a2
Michael Lobman <michael.lobman@gmail.com>
2022-04-19 20:07:30 -0400

    Add styling, functionality for Exercise Form

--- client/src/components/SetListItem.js
+++ client/src/components/SetListItem.js
@@ -1,47 +1,67 @@
+import { useState } from 'react';
+
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function SetListItem({ set, wMax, completed, setProgressions, id, userId }){
 
     const { num, reps, percentage, week_id: week } = set
+    const [ toggleChecked, setToggleChecked ] = useState(false)
     const liftingWeight = Math.round(wMax * percentage / 5) * 5
     const amrap = num === 3 && week % 4 !== 0 ? '+' : null
     const variant = completed >= num ? 'success' : 'outline-success'
     const disable = completed >= num ? true : false;
-    
-    function handleClick(e){
-        e.preventDefault();
 
-        console.log(`completed currently holds a value of ${completed}`)
-        const data = {
-            sets_completed: completed + 1
+    const toggleClass = toggleChecked ? 'green-background' : null
+
+    // function handleClick(e){
+    //     e.preventDefault();
+
+    //     console.log(`completed currently holds a value of ${completed}`)
+    //     const data = {
+    //         sets_completed: completed + 1
+    //     }
+    //     fetch(`/progressions/user/${userId}/${id}`, {
+    //         method: "PATCH",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify(data)
+    //     })
+    //         .then(r => r.json())
+    //         .then(data => setProgressions(data))
+
+    //     console.log(`completed currently holds a value of ${completed}`)
+
+    // }
+
+    function handleChange(){
+        setToggleChecked(!toggleChecked)
+        if (num === 3) {
+            console.log('this is the end')
         }
-        fetch(`/progressions/user/${userId}/${id}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-            .then(r => r.json())
-            .then(data => setProgressions(data))
-
-        console.log(`completed currently holds a value of ${completed}`)
-
     }
 
     
     return (
-        <ListGroup.Item>
+        <ListGroup.Item className={toggleClass}>
             {reps} {amrap} reps @ {liftingWeight} pounds
-            <Button 
+            <Form.Check
+                type="switch"
+                label='Completed'
+                checked={toggleChecked}
+                onChange={handleChange}
+                inline
+            />
+            {/* <Button 
                 className='set-button'
                 disabled={disable}
                 variant={variant} 
                 onClick={handleClick}
             >
                 Complete
-            </Button>
+            </Button> */}
         </ListGroup.Item>
 
     )
