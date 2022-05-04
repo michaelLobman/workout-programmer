4b895cff791e6aa5cb7224044278b7918f1ea8c8
Michael Lobman <michael.lobman@gmail.com>
2022-05-04 15:06:47 -0400

    Add conditional to migration file

--- db/migrate/20220503225619_add_current_week_to_users.rb
+++ db/migrate/20220503225619_add_current_week_to_users.rb
@@ -1,5 +1,7 @@
 class AddCurrentWeekToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :current_week, :integer
+    unless column_exists? :users, :current_week
+      add_column :users, :current_week, :integer
+    end
   end
 end
