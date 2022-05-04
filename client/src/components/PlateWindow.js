c7b83f7d04960ba6c668b1769808ac571a5e5960
Michael Lobman <michael.lobman@gmail.com>
2022-05-04 12:42:09 -0400

    Update PlateWindow component to use react-bootstrap dropdown

--- client/src/components/PlateWindow.js
+++ client/src/components/PlateWindow.js
@@ -1,22 +1,35 @@
 import Container from 'react-bootstrap/Container';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function PlateWindow({ plates }){
 
-    const fortyFive = plates[45] ? <li>45 lb: {plates[45]}</li>: null
-    const thirtyFive = plates[35] ? <li>35 lb: {plates[35]}</li> : null
-    const twentyFive = plates[25] ? <li>25 lb: {plates[25]}</li>: null
-    const ten = plates[10] ? <li>10 lb: {plates[10]}</li>: null
-    const five = plates[5] ? <li>5lb: {plates[5]}</li>: null
-    const twoHalf = plates[2.5] ? <li>2.5 lb: {plates[2.5]}</li> : null
+    const fortyFive = plates[45] ? <Dropdown.Item>45 lb: {plates[45]}</Dropdown.Item>: null
+    const thirtyFive = plates[35] ? <Dropdown.Item>35 lb: {plates[35]}</Dropdown.Item> : null
+    const twentyFive = plates[25] ? <Dropdown.Item>25 lb: {plates[25]}</Dropdown.Item>: null
+    const ten = plates[10] ? <Dropdown.Item>10 lb: {plates[10]}</Dropdown.Item>: null
+    const five = plates[5] ? <Dropdown.Item>5lb: {plates[5]}</Dropdown.Item>: null
+    const twoHalf = plates[2.5] ? <Dropdown.Item>2.5 lb: {plates[2.5]}</Dropdown.Item> : null
 
 
     return(
-        <Container>
-            <h3 id="plate-h3">Plates per side:</h3>
-            <ul id="plate-ul">
-                {fortyFive}{thirtyFive}{twentyFive}{ten}{five}{twoHalf}
-            </ul>
-        </Container>
+        <DropdownButton variant="danger" className="plates-dropdown" title="View Plates / Side">
+            {fortyFive}{thirtyFive}{twentyFive}{ten}{five}{twoHalf}
+        </DropdownButton>
+        // <Dropdown>
+        //     <Dropdown.Toggle variant="danger" className="plates-dropdown">
+        //         View Plates
+        //     </Dropdown.Toggle>
+        //     <Dropdown.Menu>
+        //         Dro
+        //     </Dropdown.Menu>
+        // </Dropdown>
+        // <Container>
+        //     <h3 id="plate-h3">Plates per side:</h3>
+        //     <ul id="plate-ul">
+        //         {fortyFive}{thirtyFive}{twentyFive}{ten}{five}{twoHalf}
+        //     </ul>
+        // </Container>
     )
 }
 
